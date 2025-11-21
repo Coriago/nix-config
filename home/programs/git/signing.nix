94aafdf856d6c262a6555d888f9fadb6edac450f
helios <gagemiller155@gmail.com>
2025-11-21 17:02:04 -0500

    Add steam gamescope

--- home/programs/git/signing.nix
+++ home/programs/git/signing.nix
@@ -2,7 +2,6 @@
 {
   # Obviously, change this to your own SSH key.
   home.file.".ssh/allowed_signers".text = "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINhWby7lUUXQNKbRu9/UOrGjWDf3fvoAwGHomWv/+lL";
-
   programs.git.settings = {
     # commit.gpgsign = true;
     gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
