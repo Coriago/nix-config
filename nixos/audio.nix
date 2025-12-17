895d68f96193c5d1b85405e5210912b5e572088b
helios <gagemiller155@gmail.com>
2025-12-17 17:06:32 -0500

    Fix audio stutter

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -9,11 +9,35 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    # extraConfig.pipewire."92-low-latency" = {
+    #   "context.properties" = {
+    #     "default.clock.rate" = 48000;
+    #     "default.clock.quantum" = 32;
+    #     "default.clock.min-quantum" = 32;
+    #     "default.clock.max-quantum" = 32;
+    #   };
+    # };
     wireplumber = {
       enable = true;
       extraConfig = {
-        "10-disable-camera" = {
-          "wireplumber.profiles" = {main."monitor.libcamera" = "disabled";};
+        "99-disable-suspend" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "node.name" = "~alsa_input.*";
+                }
+                {
+                  "node.name" = "~alsa_output.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+              };
+            }
+          ];
         };
       };
     };
