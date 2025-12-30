d7f339864fd4cb8374ce371c76d3ef214fdaf1fb
helios <gagemiller155@gmail.com>
2025-12-30 01:32:36 -0500

    fix new ext path for vscode

--- modules/features/development/ide.nix
+++ modules/features/development/ide.nix
@@ -2,7 +2,7 @@
   flake.modules.homeManager.development = {pkgs, ...}: {
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
+      profiles.default.extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         christian-kohler.path-intellisense
       ];
