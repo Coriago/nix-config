6bf22a3c62ec592d1d5763de42a68a24fa6220e8
helios <gagemiller155@gmail.com>
2026-01-01 20:41:03 -0500

    add browser env var

--- modules/features/desktop/browser.nix
+++ modules/features/desktop/browser.nix
@@ -1,4 +1,10 @@
 {
+  flake.modules.nixos.desktop = {...}: {
+    environment.variables = {
+      BROWSER = "brave";
+    };
+  };
+
   flake.modules.homeManager.desktop = {...}: {
     programs.brave = {
       enable = true;
