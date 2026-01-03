a0d59be2f90bdbe299be6bff4c2e922263898cfe
helios <gagemiller155@gmail.com>
2026-01-02 21:15:57 -0500

    keep xdg commentted out

--- modules/features/desktop/flatpak.nix
+++ modules/features/desktop/flatpak.nix
@@ -19,9 +19,7 @@
   };
 
   flake.modules.homeManager.desktop = {...}: {
-    # xdg.userDirs.enable = true;
-
-    # # Add extra XDG_DATA_DIRS for flatpak apps
+    # Add extra XDG_DATA_DIRS for flatpak apps
     # home.sessionVariables = {
     #   XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
     # };
