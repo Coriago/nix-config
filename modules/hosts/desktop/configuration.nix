93514aa288768f9efa5a0376b3ef79f0ce9fde55
helios <gagemiller155@gmail.com>
2026-01-05 23:02:22 -0500

    rename folder to desktop

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # Global Variables module
-  # reusable for different configurations
+  ############################
   flake.modules.generic.heliosdesk = {
     imports = with config.flake.modules; [
       generic.variables
@@ -16,59 +16,44 @@
       timeZone = "America/New_York";
       locale = "en_US.UTF-8";
       email = "gagemiller155@gmail.com";
+      theme = "equilibrium-dark";
+      wallpaper = "https://getwallpapers.com/wallpaper/full/2/e/a/524916.jpg";
+      wallpaperHash = "sha256-YqFWMRjdM8dGbSJQomvoNtwmq2ppfwq6r0UYYpx6sVA=";
       monitors = {
         "left" = {
-          width = 2560;
-          height = 1440;
-          refreshRate = 144;
-          x = 0;
-          y = 0;
           serial = "1APR9UA001811";
         };
         "center" = {
-          primary = true;
-          width = 2560;
-          height = 1440;
-          refreshRate = 144;
-          x = 2560;
-          y = 0;
           serial = "2OMR6UA000303";
         };
         "right" = {
-          width = 5120;
-          height = 1440;
-          refreshRate = 165;
-          x = 0;
-          y = 0;
           serial = "1APR9UA001815";
-        };
-        "tv" = {
-          width = 1920;
-          height = 1080;
-          refreshRate = 60;
-          replicaOf = "center";
         };
       };
     };
   };
 
   # Nixos host configuration
+  ###############################
   flake.modules.nixos.heliosdesk = {lib, ...}: {
+    # Import nixos modules for this host
     imports = with config.flake.modules; [
-      # Import nixos modules for this host
       generic.heliosdesk
       nixos.base
       nixos.desktop
       nixos.gaming
+
+      # Drivers
       nixos.bluetooth
       nixos.gpu
       nixos.boot
       nixos.audio
+      nixos.monitor
     ];
 
     home-manager.users.helios = {
+      # Import Home Manager modules
       imports = with config.flake.modules; [
-        # Import Home Manager modules
         generic.heliosdesk
         homeManager.base
         homeManager.development
@@ -77,22 +62,24 @@
     };
 
     # Host Overrides
-    ############################
+    #----------------------------------#
     networking.hostName = "heliosdesk";
     boot.loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       grub.enable = lib.mkForce false;
     };
+
     # Disable integrated AMD iGPU
     boot.blacklistedKernelModules = ["amdgpu"];
     boot.kernelParams = ["module_blacklist=amdgpu"];
   };
 
   # Final Configuration
+  ######################
   flake.nixosConfigurations.heliosdesk = inputs.nixpkgs.lib.nixosSystem {
     modules = [
-      config.flake.modules.nixos.heliosdesk # The module defined below
+      config.flake.modules.nixos.heliosdesk # The module defined above
     ];
   };
 }
