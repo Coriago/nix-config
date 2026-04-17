a98903ec35a4435dfc241cf7e00a18cd3b333d86
helios <gagemiller155@gmail.com>
2026-04-16 20:38:39 -0400

    Add kdeconnect and partition manager to desktop

--- modules/desktop/desktop.nix
+++ modules/desktop/desktop.nix
@@ -12,6 +12,8 @@
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
+    programs.kdeconnect.enable = true;
+    programs.partition-manager.enable = true;
 
     # Enable x11
     services.xserver = {
