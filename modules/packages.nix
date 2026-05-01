8cfd82955602853d0296057a369bedaa15d968ba
helios <gagemiller155@gmail.com>
2026-05-01 00:31:09 -0400

    remove gaomon

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,6 +1,5 @@
 {inputs, ...}: {
   perSystem = {pkgs, ...}: {
     packages.comment-checker = pkgs.callPackage ../packages/comment-checker.nix {};
-    packages.gaomon-driver = pkgs.callPackage ../packages/gaomon-driver.nix {};
   };
 }
